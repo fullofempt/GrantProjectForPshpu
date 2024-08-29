58767b6db229cbd24c5bbc7c9314d55b980972bf
fullofempt <super.shlykov11@yandex.ru>
2024-08-29 14:24:44 +0500

    test123

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter/services.dart';
 import 'package:testik/app/routes/app_pages.dart';
 
 void main() {
-  runApp(GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: Routes.SPLASH,
-    defaultTransition: Transition.circularReveal,
-    getPages: AppPages.routes,
-  ));
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.SPLASH,
+      defaultTransition: Transition.circularReveal,
+      getPages: AppPages.routes,
+    ));
+  });
 }
